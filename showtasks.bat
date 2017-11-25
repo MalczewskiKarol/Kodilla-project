425aeee90a2d8ca6e090fe403a59ce69bfff8916
MalczewskiKarol <karolmalczewski2@o2.pl>
2017-11-25 02:26:28 +0100

    17.4

--- showtasks.bat
+++ showtasks.bat
@@ -1,11 +1,18 @@
 call runcrud.bat
-if "%ERRORLEVEL%" == "0" goto startchrome
+if "%ERRORLEVEL%" == "0" goto wait30sec
 echo.
 echo Cannot run runcrud.bat - APLICATION STOPPED
 goto fail
 
+:wait30sec
+sleep 30
+if "%ERRORLEVEL%" == "0" goto startchrome
+echo.
+echo Cannot wait 25 sec - APPLICATION STOPPED
+goto fail
+
 :startchrome
-start start chrome "http://localhost:8080/crud/v1/task/getTasks"
+start chrome "http://localhost:8080/crud/v1/task/getTasks"
 if "%ERRORLEVEL%" == "0" goto end
 echo.
 echo Cannot go to required URL - APPLICATION STOPPED
