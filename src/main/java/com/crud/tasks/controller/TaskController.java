15c48f548c85642145215c90ab169019bb55ad92
MalczewskiKarol <karolmalczewski2@o2.pl>
2018-01-25 23:27:50 +0100

    24.4

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -30,7 +30,7 @@ public class TaskController {
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "/tasks/{taskId}")
-    public void deleteTask(@PathVariable Long taskId) throws TaskNotFoundException {
+    public void deleteTask(@PathVariable Long taskId) {
         service.deleteTask(taskId);
     }
 
